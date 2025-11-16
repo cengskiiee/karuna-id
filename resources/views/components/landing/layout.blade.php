e783ba09f1e1fd2597dc1642f6a43dba70ceed51
RynxDev <rynxdev@RynxDevs-MacBook-Pro.local>
2025-11-16 22:37:38 +0700

    update

--- resources/views/components/landing/layout.blade.php
+++ resources/views/components/landing/layout.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hadiwana | @yield('title')</title>
+    <title>Karuna ID | @yield('title')</title>
     @include('components.landing.styles')
 </head>
+
 <body>
 
     @include('components.landing.header')
@@ -20,4 +22,5 @@
 
 
 </body>
+
 </html>
